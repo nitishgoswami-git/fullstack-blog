397a9ede16b269dc4edd6a7ea0e48c0426aa1d8d
Nitish Goswami <itsgoswaminitish@gmail.com>
2025-06-08 14:22:00 +0000

    feat: integrate Tailwind CSS and Vite, add Header and Footer components, implement authentication state management
    - Added Tailwind CSS and Vite dependencies in package.json.
    - Created Header and Footer components for improved layout.
    - Implemented authentication state management using Redux with authSlice.
    - Updated App component to handle user authentication state.
    - Configured Vite to use Tailwind CSS.
    - Removed outdated README.md file.

--- Fullstack_Blog/src/App.jsx
+++ Fullstack_Blog/src/App.jsx
@@ -1,14 +1,31 @@
+import { useEffect } from 'react'
 import './App.css'
 import conf from './conf/conf'
 
 function App() {
-  console.log(conf.appwriteUrl)
+  const [loading, setLoading] = useState(true)
+  const dispatch = useDispatch()
 
-  return (
-    <>
-     <h1>Appwrite</h1>
-    </>
-  )
+
+  useEffect(() => {
+    authService.getCurrentUser()
+      .then((user) => {
+        if (user) {
+          dispatch(login({ userData: user }))
+        } else {
+          dispatch(logout())
+        }
+      })
+      .catch((error) => {
+        console.error("Error fetching current user:", error)
+      })
+      .finally(() => {
+        setLoading(false)
+      })
+  }, [dispatch])
+  if (loading) {
+    return <div className='min-h-screen flex flex-wrap'>Loading...</div>
+  }
 }
 
 export default App
